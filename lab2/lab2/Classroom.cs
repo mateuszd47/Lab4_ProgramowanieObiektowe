af9367e606370d4de3afd8cf215d5562be937bdf
mdynur <91785121+mateuszd47@users.noreply.github.com>
2022-03-31 19:54:19 +0200

    Add files

---
+++ lab2/lab2/Classroom.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2
+{
+    class Classroom
+    {
+        private string name;
+        private Person[] persons;
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public Classroom(string name, Person[] persons)
+        {
+            this.name = name;
+            this.persons = persons;
+        }
+        override public string ToString()
+        {
+            string result = "Classroom: " + this.name + "\n\n";
+            foreach(var p in persons)
+            {
+                result += p.ToString() + "\n";
+            }
+            return result;
+        }
+    }
+}
---
+++ lab2/lab2/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2
+{
+    class Person
+    {
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        protected string name;
+        protected int age;
+        public string Name
+        {
+            get { return name; }
+        }
+        public int Age
+        {
+            get { return age; }
+        }
+
+        public bool Equals(Person obj)
+        {
+            return obj.name == this.name && obj.age == this.age;
+        }
+
+        override public string ToString()
+        {
+            return this.name + "(" + this.age.ToString() + " y.o.)";
+        }
+    }
+}
